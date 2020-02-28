8d13d710589b3b03fb9a0fb2972c2a29fae6d564
David Oster <paspa@hotmail.com>
2020-02-28 06:56:30 +0200

    Update MoveService.java
    Latest version with all the logic implemented

--- src/java/services/MoveService.java
+++ src/java/services/MoveService.java
@@ -19,9 +19,52 @@ public class MoveService implements IMoveService {
 
     @Override
     public boolean save(int position, String symbol) {
+        // business login
+        // we should accept only 0-8 for position
+        /*
+        | 0 - 0,0 | 1 - 0,1 | 2 - 0,2 | 
+        | 3 - 1,0 | 4 - 1,1 | 5 - 1,2 | 
+        | 6 - 2,0 | 7 - 2,1 | 8 - 2,2 | 
+        */
         Cell cell = new Cell();
-        cell.setX(position);
-        cell.setY(position);
+        switch(position) {
+            case 0:
+                cell.setX(position);
+                cell.setY(position);
+                break;
+            case 1:
+                cell.setX(0);
+                cell.setY(1);
+                break;
+            case 2:
+                cell.setX(0);
+                cell.setY(2);
+                break;
+            case 3:
+                cell.setX(1);
+                cell.setY(0);
+                break;
+            case 4:
+                cell.setX(1);
+                cell.setY(1);
+                break;
+            case 5:
+                cell.setX(1);
+                cell.setY(2);
+                break;
+            case 6:
+                cell.setX(2);
+                cell.setY(0);
+                break;
+            case 7:
+                cell.setX(2);
+                cell.setY(1);
+                break;
+            case 8:
+                cell.setX(2);
+                cell.setY(2);
+                break;
+        }
         Symbol aSymbol = new Symbol(symbol);
         Move move = new Move();
         move.setX(cell.getX());
